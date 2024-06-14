84f5fd8cd88835959b74f20eae081b8dcef561d5
kingsjaymes76@gmail.com <155890307+Jamon745@users.noreply.github.com>
2024-06-14 06:35:01 -0700

    updated

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -7,7 +7,7 @@ import Button from "./Button";
 
 const Overview = () => {
   return (
-    <div className="flex">
+    <div className="flex max-w-[100vw]">
       <SideBar />
       <div>
         <NavBar />
